066fd9fca44f3aa5a425d9bec89e3f21355e5b9d
BenAli Samar <samar.benali@istic.ucar.tn>
2024-02-25 21:24:46 +0100

    Refactoring

--- video.c
+++ video.c
@@ -8,58 +8,91 @@
 #include <linux/videodev2.h>
 
 #define BUFFER_COUNT 2
+#define DESIRED_FRAME_RATE 50
+#define POLL_TIMEOUT_US (1000000 / DESIRED_FRAME_RATE)
 
+struct buffer {
+    void *start;
+    size_t length;
+};
 
+// Cleanup stuff
+void cleanup(int fd, int out_fd, struct buffer *buffers, int buffer_count) {
+    enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    ioctl(fd, VIDIOC_STREAMOFF, &type);
 
-
-int main(int argc, char **argv)
-{
-    // Open the video capture device 
-    int fd = open("/dev/video0", O_RDWR |O_NONBLOCK);
-   
-    if (fd < 0) {
-        perror("Failed to open video capture device");
-        exit(1);
+    for (int i = 0; i < buffer_count; i++) {
+        if (buffers[i].start != MAP_FAILED) {
+            munmap(buffers[i].start, buffers[i].length);
+        }
     }
 
-    // Here Setting the video capture device format and parameters
+    if (fd >= 0) {
+        close(fd);
+    }
+    if (out_fd >= 0) {
+        close(out_fd);
+    }
+
+    if (buffers != NULL) {
+        free(buffers);
+    }
+}
+
+
+int main(int argc, char **argv) {
+    int fd = -1;
+    int out_fd = -1;
+    struct buffer *buffers = NULL;
+
+    // Input file
+    fd = open("/dev/video0", O_RDWR | O_NONBLOCK);
+    if (fd < 0) {
+        perror("Failed to open video capture device");
+        exit(EXIT_FAILURE);
+    }
+
+    // Setting the video capture device format and parameters
     struct v4l2_format fmt = {0};
     fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     fmt.fmt.pix.width = 640;
     fmt.fmt.pix.height = 480;
-    fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG;	//V4L2_PIX_FMT_H264; 	// for raw format
-    fmt.fmt.pix.field =V4L2_FIELD_NONE; 	//V4L2_FIELD_ANY;	// for raw format
+    fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG;  
+    fmt.fmt.pix.field = V4L2_FIELD_NONE;          
     if (ioctl(fd, VIDIOC_S_FMT, &fmt) < 0) {
         perror("Failed to set video format");
-        		// if i change the loopback device it gives device busy (business man :P )
-        		//  ==> sorry i am the dumb here, business man, i thought u a multitasker hhh
-        exit(1);
+        cleanup(fd, out_fd, buffers, BUFFER_COUNT);
+        exit(EXIT_FAILURE);
     }
 
-    // The output device,  i'm creating a new file to output it 
-    int out_fd = open("out.mp4", O_CREAT | O_WRONLY | O_TRUNC |O_NONBLOCK, 0644); // it can be /dev/video1 or out.raw
+    // The output file
+    out_fd = open("out.mp4", O_CREAT | O_WRONLY | O_TRUNC | O_NONBLOCK, 0644);
     if (out_fd < 0) {
         perror("Failed to open output device");
-        exit(1);
+        cleanup(fd, out_fd, buffers, BUFFER_COUNT);
+        exit(EXIT_FAILURE);
     }
 
-    // Allocate buffers for the video capture device
-    // source : nhttps://www.linuxtv.org/downloads/v4l-dvb-apis-old/mmap.html
+    // Allocate memory for the video buffers
     struct v4l2_requestbuffers req = {0};
     req.count = BUFFER_COUNT;
-    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;  // allocate device buffers 
-    req.memory = V4L2_MEMORY_MMAP;  	    // memory mapped buffers allocated in kernel
+    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;  
+    req.memory = V4L2_MEMORY_MMAP;           // Memory mapped buffers allocated in kernel
     if (ioctl(fd, VIDIOC_REQBUFS, &req) < 0) {
         perror("Failed to allocate buffers");
-        exit(1);
+        cleanup(fd, out_fd, buffers, BUFFER_COUNT);
+        exit(EXIT_FAILURE);
     }
 
-    		//  buffers specifications and map them into their @ space with mmap() 
-    struct buffer {
-        void *start;
-        size_t length;
-    } *buffers;
-    buffers = calloc(req.count, sizeof(*buffers));
+    // Allocate memory for buffer pointers
+    buffers = calloc(req.count, sizeof(struct buffer));
+    if (!buffers) {
+        perror("Failed to allocate memory for buffers");
+        cleanup(fd, out_fd, buffers, BUFFER_COUNT);
+        exit(EXIT_FAILURE);
+    }
+
+    // Map buffers into memory
     for (int i = 0; i < req.count; i++) {
         struct v4l2_buffer buf = {0};
         buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
@@ -67,25 +100,21 @@ int main(int argc, char **argv)
         buf.index = i;
         if (ioctl(fd, VIDIOC_QUERYBUF, &buf) < 0) {
             perror("Failed to query buffer");
-            exit(1);
+            cleanup(fd, out_fd, buffers, BUFFER_COUNT);
+            exit(EXIT_FAILURE);
         }
         buffers[i].length = buf.length;
         buffers[i].start = mmap(NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
         if (buffers[i].start == MAP_FAILED) {
             perror("Failed to map buffer");
-            exit(1);
+            cleanup(fd, out_fd, buffers, BUFFER_COUNT);
+            exit(EXIT_FAILURE);
         }
-    }
-
-    // Enqueue the video buffers before processing
-    for (int i = 0; i < req.count; i++) {
-        struct v4l2_buffer buf = {0};
-        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        buf.memory = V4L2_MEMORY_MMAP;
-        buf.index = i;
+        // Enqueue the video buffer
         if (ioctl(fd, VIDIOC_QBUF, &buf) < 0) {
             perror("Failed to enqueue buffer");
-            exit(1);
+            cleanup(fd, out_fd, buffers, BUFFER_COUNT);
+            exit(EXIT_FAILURE);
         }
     }
 
@@ -93,7 +122,8 @@ int main(int argc, char **argv)
     enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     if (ioctl(fd, VIDIOC_STREAMON, &type) < 0) {
         perror("Failed to start capturing");
-        exit(1);
+        cleanup(fd, out_fd, buffers, BUFFER_COUNT);
+        exit(EXIT_FAILURE);
     }
 
     // Use poll to wait for video capture events 
@@ -102,11 +132,11 @@ int main(int argc, char **argv)
     poll_fds[0].events = POLLIN;
 
     while (1) {
-        int ret = poll(poll_fds, 1, 1000); // wait up to 1 second for events 
-        				  // ( put -1 if it should wait forever )
+        int ret = poll(poll_fds, 1, POLL_TIMEOUT_US); // Wait up to 1 second for events 
         if (ret < 0) {
             perror("Failed to poll");
-            exit(1);
+            cleanup(fd, out_fd, buffers, BUFFER_COUNT);
+            exit(EXIT_FAILURE);
         } else if (ret == 0) {
             printf("Poll timeout\n");
             continue;
@@ -119,7 +149,8 @@ int main(int argc, char **argv)
             buf.memory = V4L2_MEMORY_MMAP;
             if (ioctl(fd, VIDIOC_DQBUF, &buf) < 0) {
                 perror("Failed to dequeue buffer");
-                exit(1);
+                cleanup(fd, out_fd, buffers, BUFFER_COUNT);
+                exit(EXIT_FAILURE);
             }
 
             // Write the video data to the output device
@@ -128,31 +159,15 @@ int main(int argc, char **argv)
             // Enqueue the video buffer again
             if (ioctl(fd, VIDIOC_QBUF, &buf) < 0) {
                 perror("Failed to enqueue buffer");
-                exit(1);
+                cleanup(fd, out_fd, buffers, BUFFER_COUNT);
+                exit(EXIT_FAILURE);
             }
+            
+            usleep(1000000 / DESIRED_FRAME_RATE);
         }
     }
 
-    // Stop capturing video frames
-    if (ioctl(fd, VIDIOC_STREAMOFF, &type) < 0) {
-        perror("Failed to stop capturing");
-        exit(1);
-    }
-
-    // Unmap/free the video buffers
-    for (int i = 0; i < req.count; i++) {
-        munmap(buffers[i].start, buffers[i].length);
-    }
-
-    // Close the video capture device and the output device
-    close(fd);
-    close(out_fd);
-
+    cleanup(fd, out_fd, buffers, BUFFER_COUNT);
     return 0;
 }
-
-// To do :  
-//	- Write the tools/cmds used to test 
-//	- Try to add/implement events by v4l2_subscribe_event
-//	- Try to put capturing/buffering in a thread by pipe 
 
